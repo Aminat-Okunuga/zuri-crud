2d567ba43f64f6bc7f83234c48c486a4867cb1e0
Aminat-Okunuga <makadeaminat@gmail.com>
2021-05-10 11:16:25 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require ('express')
-const mongoose  = require('mongoose')
-const cors = require('cors')
-const port = process.env.port || 22100
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.port || 22100;
 // db url
-const database = "mongodb+srv://AminatOkunuga:AminatOkunuga@cluster0.l0t8b.mongodb.net/school?retryWrites=true&w=majority"
+const database =
+  "mongodb+srv://AminatOkunuga:AminatOkunuga@cluster0.l0t8b.mongodb.net/school?retryWrites=true&w=majority";
 
 // server declaration
-const app  = express()
-app.use(express.json)
+const app = express();
+app.use(express.json);
 
 // connect to db
-mongoose.connect(database,{
+mongoose
+  .connect(database, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Connection Established!")
-})
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connection Established!");
+  });
+  
 // use cors
-app.use(cors())
-app.use("/", require('./controller/controller'))
+app.use(cors());
+app.use("/", require("./controller/controller"));
+
 // listen to port
 app.listen(port, () => {
-    console.log(`${port}`)
-})
+  console.log(`${port}`);
+});
